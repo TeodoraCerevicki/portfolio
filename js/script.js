c69f61fb39a637e187abb51b5ff4289a16670887
teodora cerevicki <teodora.cerevicki@gmail.com>
2023-11-15 15:47:17 +0100

    mobile nav toggle

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
 $(function(){
+    var width = $(window).width();
+    
     $('.js-card').hover(function(){
         $('.js-card-content', this).slideToggle('open');
+    });
+
+    $('.js-close-icon, .js-burger-icon, .js-nav').click(function(){
+        if (width < 1024) {
+            $('.js-burger-icon, .js-close-icon, .js-nav').toggleClass('hide');
+            $('body').toggleClass('open-menu');
+        }
     });
 });
