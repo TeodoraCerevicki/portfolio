eead13579b6fe9a8f0e260de542ea26367592dbb
teodora cerevicki <teodora.cerevicki@gmail.com>
2023-11-16 11:15:20 +0100

    experiance responsive

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
 $(function(){
     var width = $(window).width();
     
-    $('.js-card').hover(function(){
-        $('.js-card-content', this).slideToggle('open');
-    });
+    if (width >= 1024) {
+        $('.js-card').hover(function(){
+            $('.js-card-content, .js-card-banner', this).slideToggle('open');
+        });
+    }
+    
+    if (width < 1024) {
+        $('.js-card').click(function(){
+            $('.js-card-content', this).slideToggle('open');
+        });
+    }
 
     $('.js-close-icon, .js-burger-icon, .js-nav').click(function(){
         if (width < 1024) {
