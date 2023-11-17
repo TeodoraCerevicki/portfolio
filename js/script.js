25ecde3650f33e805991a64c8f53dcf5de14b46f
teodora cerevicki <teodora.cerevicki@gmail.com>
2023-11-17 15:29:24 +0100

    experience update

--- js/script.js
+++ js/script.js
@@ -1,17 +1,5 @@
 $(function(){
     var width = $(window).width();
-    
-    if (width >= 1024) {
-        $('.js-card').hover(function(){
-            $('.js-card-content, .js-card-banner', this).slideToggle('open');
-        });
-    }
-    
-    if (width < 1024) {
-        $('.js-card').click(function(){
-            $('.js-card-content', this).slideToggle('open');
-        });
-    }
 
     $('.js-close-icon, .js-burger-icon, .js-nav').click(function(){
         if (width < 1024) {
