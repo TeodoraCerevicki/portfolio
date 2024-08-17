8b96dfaf66e3ed122bdb3b39c8f2ccbed0ec4a72
teodora cerevicki <teodora.cerevicki@gmail.com>
2024-08-17 11:30:20 +0200

    animation on scroll

--- js/script.js
+++ js/script.js
@@ -48,3 +48,27 @@ window.onload = function() {
         }, 10000);
     });
 }
+
+const animationOnScroll = gsap.utils.toArray('.js-animation');
+
+animationOnScroll.forEach(item => {
+    const anim = gsap.fromTo(
+        item, {
+            autoAlpha: 0,
+            y: 100,
+            x: -100,
+            rotate: -10
+        },
+        {
+            duration: 0.6,
+            autoAlpha: 1,
+            y: 0,
+            x: 0,
+            rotate: 0
+        }
+    );
+    ScrollTrigger.create({
+        trigger: item,
+        animation: anim,
+    });
+});
